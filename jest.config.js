c41e8b6121ed3b803fed58253feced3e4ef536a2
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-02 14:58:21 -0300

    chore: add initial packages and base configurations styles

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const { compilerOptions } = require('./tsconfig.json')
+
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: [
+    "text-summary",
+    "lcov",
+  ],
+  preset: 'ts-jest',
+  testEnvironment: "node",
+  testMatch: [
+    "**/*.spec.ts"
+ ],
+};
