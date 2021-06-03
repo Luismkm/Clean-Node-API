f3d0dba407478c44d31290ed1f8495afb3d49b23
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-03 19:07:35 -0300

    test: remove aysnc/await

--- src/utils/EmailValidatorAdapter.spec.ts
+++ src/utils/EmailValidatorAdapter.spec.ts
@@ -2,17 +2,17 @@ import validator from 'validator';
 import { EmailValidatorAdapter } from './EmailValidatorAdapter';
 
 describe('EmailValidator Adapter', () => {
-  it('Should return false if validator returns false', async () => {
+  it('Should return false if validator returns false', () => {
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
 
     const sut = new EmailValidatorAdapter();
-    const isValid = await sut.isValid('invalid_email@mail.com');
+    const isValid = sut.isValid('invalid_email@mail.com');
     expect(isValid).toBe(false);
   });
 
-  it('Should return false if validator returns true', async () => {
+  it('Should return false if validator returns true', () => {
     const sut = new EmailValidatorAdapter();
-    const isValid = await sut.isValid('valid_email@mail.com');
+    const isValid = sut.isValid('valid_email@mail.com');
     expect(isValid).toBe(true);
   });
 });
