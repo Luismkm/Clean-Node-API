9a3f97cf6a46129ff4280389d37cca9a5e6a7e63
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-04 20:41:30 -0300

    test: ensure DbCreateAccount calls CreateAccountRepository with correct values

--- src/data/usecases/create-account/DbCreateAccount.spec.ts
+++ src/data/usecases/create-account/DbCreateAccount.spec.ts
@@ -1,21 +1,41 @@
 import { DbCreateAccount } from './DbCreateAccount';
+import {
+  IEncrypter,
+  ICreateAccountDTO,
+  IAccount,
+  ICreateAccountRepository,
+} from './DbCreateAccountProtocols';
 
-let sut: DbCreateAccount;
-let encrypterStub;
-
-class EncrypterStub {
+class EncrypterStub implements IEncrypter {
   async encrypt(value: string): Promise<string> {
     return new Promise((resolve) => resolve('hashed_password'));
   }
 }
 
+class CreateAccountRepositoryStub implements ICreateAccountRepository {
+  async create(account: ICreateAccountDTO): Promise<IAccount> {
+    const fakeAccount = {
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password',
+    };
+    return new Promise((resolve) => resolve(fakeAccount));
+  }
+}
+
+let sut: DbCreateAccount;
+let encrypterStub: EncrypterStub;
+let createAccountRepositoryStub: CreateAccountRepositoryStub;
+
 describe('DbCreateAccount Usecase', () => {
   beforeEach(() => {
     encrypterStub = new EncrypterStub();
-    sut = new DbCreateAccount(encrypterStub);
+    createAccountRepositoryStub = new CreateAccountRepositoryStub();
+    sut = new DbCreateAccount(encrypterStub, createAccountRepositoryStub);
   });
 
-  it('Should call Encrypter with correct passowrd', async () => {
+  it('Should call Encrypter with correct password', async () => {
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
     const accountData = {
       name: 'valid_name',
@@ -37,5 +57,20 @@ describe('DbCreateAccount Usecase', () => {
     };
     const promise = sut.create(accountData);
     await expect(promise).rejects.toThrow();
+  });
+
+  it('Should call CreateAccountRepository with correct values', async () => {
+    const createSpy = jest.spyOn(createAccountRepositoryStub, 'create');
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+    };
+    await sut.create(accountData);
+    expect(createSpy).toHaveBeenCalledWith({
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password',
+    });
   });
 });
