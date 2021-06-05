3465826cac4b3907a21bbb1078a65e322c6448d3
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-05 11:05:26 -0300

    feat: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/BcryptAdapter.ts
+++ src/infra/criptography/BcryptAdapter.ts
@@ -9,7 +9,7 @@ export class BcryptAdapter implements IEncrypter {
   }
 
   async encrypt(value: string): Promise<string> {
-    bcrypt.hash(value, this.salt);
-    return new Promise((resolve) => resolve(''));
+    const hash = bcrypt.hash(value, this.salt);
+    return hash;
   }
 }
