92900a5c167fcdf2cf286a310720a96e99894f8d
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-05 10:09:41 -0300

    feat: ensure BcryptAdapter calss bcrypt with correct values

---
+++ src/infra/criptography/BcryptAdapter.ts
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt';
+import { IEncrypter } from '../../data/protocols/IEncrypter';
+
+export class BcryptAdapter implements IEncrypter {
+  private readonly salt: number
+
+  constructor(salt: number) {
+    this.salt = salt;
+  }
+
+  async encrypt(value: string): Promise<string> {
+    bcrypt.hash(value, this.salt);
+    return new Promise((resolve) => resolve(''));
+  }
+}
