075c591da24445e00894320430fa32afb2da9315
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-05 20:02:33 -0300

    chore: add mongodb jest-mongodb and configurations

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,11 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils')
-const { compilerOptions } = require('./tsconfig.json')
-
 module.exports = {
-  clearMocks: true,
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
-  ],
-  coverageDirectory: 'coverage',
-  coverageReporters: [
-    "text-summary",
-    "lcov",
-  ],
-  preset: 'ts-jest',
-  testEnvironment: "node",
+ roots: ['<rootDir>/src'],
+ collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+ coverageDirectory: 'coverage',
+ testEnvironment: 'node',
+ preset: '@shelf/jest-mongodb',
+ transform: {
+   '.+\\.ts$': 'ts-jest'
+ }
 
 };
