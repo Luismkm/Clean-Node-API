d58f49465af1b6c1b0bac3dc318eba0018237714
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-05 20:09:50 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/database/mongodb/accountRepository/account.spec.ts
@@ -0,0 +1,29 @@
+import { MongoHelper } from '../helpers/mongoHelper';
+import { AccountMongoRepository } from './Account';
+
+let sut: AccountMongoRepository;
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(() => {
+    sut = new AccountMongoRepository();
+  });
+  it('Should return an account on success', async () => {
+    const account = await sut.create({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('any_name');
+    expect(account.email).toBe('any_email@mail.com');
+  });
+});
