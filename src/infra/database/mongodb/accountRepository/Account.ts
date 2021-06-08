3e4a193d1eea82e1dca2584222948c8efc094da1
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-08 11:06:10 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/database/mongodb/accountRepository/Account.ts
+++ src/infra/database/mongodb/accountRepository/Account.ts
@@ -5,7 +5,7 @@ import { MongoHelper } from '../helpers/mongoHelper';
 
 export class AccountMongoRepository implements ICreateAccountRepository {
   async create(account: ICreateAccountDTO): Promise<IAccount> {
-    const accountCollection = MongoHelper.getCollection('accounts');
+    const accountCollection = await MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(account);
 
     return MongoHelper.map(result.ops[0]);
