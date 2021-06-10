615832018862a92dbe65770512fd6214f328efa8
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-10 19:22:22 -0300

    refactor: remove code duplication

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -3,11 +3,19 @@ import { badRequest } from '../../helpers/http-helper';
 import { InvalidParamError, MissingParamError } from '../../errors';
 
 import { IEmailValidator } from '../../protocols/IEmailValidator';
+import { IHttpRequest } from '../../protocols/IHttp';
 
 interface ISutTypes {
   sut: LoginController,
   emailValidatorStub: IEmailValidator
 }
+
+const makeFakeRequest = (): IHttpRequest => ({
+  body: {
+    email: 'any_email',
+    password: 'any_password',
+  },
+});
 
 const makeEmailValidator = (): IEmailValidator => {
   class EmailValidatorStub implements IEmailValidator {
@@ -15,7 +23,6 @@ const makeEmailValidator = (): IEmailValidator => {
       return true;
     }
   }
-
   return new EmailValidatorStub();
 };
 
@@ -56,13 +63,7 @@ describe('Login Controller', () => {
   it('Should return 400 if an invalid email is provided', async () => {
     const { sut, emailValidatorStub } = makeSut();
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
-    const httpRequest = {
-      body: {
-        email: 'any_email',
-        password: 'any_password',
-      },
-    };
-    const httpResponse = await sut.handle(httpRequest);
+    const httpResponse = await sut.handle(makeFakeRequest());
 
     expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')));
   });
@@ -70,13 +71,7 @@ describe('Login Controller', () => {
   it('Should call EmailValidator with correct email', async () => {
     const { sut, emailValidatorStub } = makeSut();
     const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid');
-    const httpRequest = {
-      body: {
-        email: 'any_email',
-        password: 'any_password',
-      },
-    };
-    await sut.handle(httpRequest);
+    await sut.handle(makeFakeRequest());
 
     expect(isValidSpy).toHaveBeenCalledWith('any_email');
   });
