afd8407d7c4d159022a26298cedb2b63dc786ee9
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-14 19:12:51 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/DbAuthentication.ts
+++ src/data/usecases/authentication/DbAuthentication.ts
@@ -2,6 +2,7 @@ import { IAuthentication, IAuthenticationDTO } from '../../../domain/usecases/IA
 import { IHashComparer } from '../../protocols/criptography/IHashComparer';
 import { ITokenGenerator } from '../../protocols/criptography/ITokenGenerator';
 import { ILoadAccountByEmailRepository } from '../../protocols/db/ILoadAccountByEmailRepository';
+import { IUpdateAccessTokenRepository } from '../../protocols/db/IUpdateAccessTokenRepository';
 
 export class DbAuthentication implements IAuthentication {
   private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository
@@ -10,14 +11,19 @@ export class DbAuthentication implements IAuthentication {
 
   private readonly tokenGenerator: ITokenGenerator
 
+  private readonly updateAccessTokenRepository: IUpdateAccessTokenRepository
+
   constructor(
     loadAccountByEmailRepository: ILoadAccountByEmailRepository,
     hashCompare: IHashComparer,
     tokenGenerator: ITokenGenerator,
+    updateAccessTokenRepository: IUpdateAccessTokenRepository,
+
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository;
     this.hashCompare = hashCompare;
     this.tokenGenerator = tokenGenerator;
+    this.updateAccessTokenRepository = updateAccessTokenRepository;
   }
 
   async auth(authentication: IAuthenticationDTO): Promise<string> {
@@ -26,6 +32,7 @@ export class DbAuthentication implements IAuthentication {
       const isValid = await this.hashCompare.compare(authentication.password, account.password);
       if (isValid) {
         const accessToken = await this.tokenGenerator.generate(account.id);
+        await this.updateAccessTokenRepository.update(account.id, accessToken);
         return accessToken;
       }
     }
