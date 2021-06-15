a8e51ce8a0ff6b1046403156adb86f9a0ecbbfdf
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-15 14:27:04 -0300

    refactor: remove sut from beforeEach to make method

--- src/infra/criptography/BcryptAdapter.spec.ts
+++ src/infra/criptography/BcryptAdapter.spec.ts
@@ -1,23 +1,25 @@
 import bcrypt from 'bcrypt';
 import { BcryptAdapter } from './BcryptAdapter';
 
-let sut: BcryptAdapter;
-const salt = 12;
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve('hash'));
+  },
+}));
 
-jest.spyOn(bcrypt, 'hash').mockImplementation(() => new Promise((resolve) => resolve('hash')));
+const salt = 12;
+const makeSut = (): BcryptAdapter => new BcryptAdapter(salt);
 
 describe('Bcrypt Adapter', () => {
-  beforeEach(() => {
-    sut = new BcryptAdapter(salt);
-  });
-
   it('should call bcrypt with correct value', async () => {
+    const sut = makeSut();
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.hash('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
   });
 
   it('should return a hash on success', async () => {
+    const sut = makeSut();
     const hash = await sut.hash('any_value');
     expect(hash).toBe('hash');
   });
