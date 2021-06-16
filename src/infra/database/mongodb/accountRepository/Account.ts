65a493279fd19b3b7d174ac26bc8a752c5019cde
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-16 09:58:45 -0300

    feat: ensure AccountMongoRepository retuns an account on loadByEmail success

--- src/infra/database/mongodb/accountRepository/Account.ts
+++ src/infra/database/mongodb/accountRepository/Account.ts
@@ -2,12 +2,20 @@ import { ICreateAccountRepository } from '../../../../data/protocols/db/ICreateA
 import { ICreateAccountDTO } from '../../../../domain/usecases/ICreateAccount';
 import { IAccount } from '../../../../domain/models/IAccount';
 import { MongoHelper } from '../helpers/mongoHelper';
+import { ILoadAccountByEmailRepository } from '../../../../data/protocols/db/ILoadAccountByEmailRepository';
 
-export class AccountMongoRepository implements ICreateAccountRepository {
+export class AccountMongoRepository implements
+  ICreateAccountRepository,
+  ILoadAccountByEmailRepository {
   async create(account: ICreateAccountDTO): Promise<IAccount> {
     const accountCollection = await MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(account);
-
     return MongoHelper.map(result.ops[0]);
+  }
+
+  async loadByEmail(email: string): Promise<IAccount> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const account = await accountCollection.findOne({ email });
+    return account && MongoHelper.map(account);
   }
 }
