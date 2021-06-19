8f3d750b6c1bb7a509997094f4874b2ed546d514
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-19 18:46:29 -0300

    refactor: alter to make sut method

--- src/data/usecases/create-account/DbCreateAccount.spec.ts
+++ src/data/usecases/create-account/DbCreateAccount.spec.ts
@@ -1,33 +1,12 @@
 import { DbCreateAccount } from './DbCreateAccount';
 
 import { ICreateAccountRepository } from '../../protocols/db/account/ICreateAccountRepository';
+import { ILoadAccountByEmailRepository } from '../authentication/DbAuthenticationProtocols';
 import {
   IHasher,
   ICreateAccountDTO,
   IAccount,
 } from './DbCreateAccountProtocols';
-
-class HasherStub implements IHasher {
-  async hash(value: string): Promise<string> {
-    return new Promise((resolve) => resolve('hashed_password'));
-  }
-}
-
-class CreateAccountRepositoryStub implements ICreateAccountRepository {
-  async create(account: ICreateAccountDTO): Promise<IAccount> {
-    const fakeAccount = {
-      id: 'valid_id',
-      name: 'valid_name',
-      email: 'valid_email',
-      password: 'hashed_password',
-    };
-    return new Promise((resolve) => resolve(fakeAccount));
-  }
-}
-
-let sut: DbCreateAccount;
-let hasherStub: HasherStub;
-let createAccountRepositoryStub: CreateAccountRepositoryStub;
 
 const makeFakeAccountDTO = (): ICreateAccountDTO => ({
   name: 'valid_name',
@@ -35,20 +14,74 @@ const makeFakeAccountDTO = (): ICreateAccountDTO => ({
   password: 'valid_password',
 });
 
-describe('DbCreateAccount Usecase', () => {
-  beforeEach(() => {
-    hasherStub = new HasherStub();
-    createAccountRepositoryStub = new CreateAccountRepositoryStub();
-    sut = new DbCreateAccount(hasherStub, createAccountRepositoryStub);
-  });
+const makeFakeAccount = (): IAccount => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email',
+  password: 'hashed_password',
+});
 
+const makeHasher = (): IHasher => {
+  class HasherStub implements IHasher {
+    async hash(value: string): Promise<string> {
+      return new Promise((resolve) => resolve('hashed_password'));
+    }
+  }
+  return new HasherStub();
+};
+
+const makeCreateAccountRepository = (): ICreateAccountRepository => {
+  class CreateAccountRepositoryStub implements ICreateAccountRepository {
+    async create(account: ICreateAccountDTO): Promise<IAccount> {
+      return new Promise((resolve) => resolve(makeFakeAccount()));
+    }
+  }
+  return new CreateAccountRepositoryStub();
+};
+
+const makeLoadAccountByEmailRepository = (): ILoadAccountByEmailRepository => {
+  class LoadAccountByEmailRepositoryStub implements ILoadAccountByEmailRepository {
+    async loadByEmail(email: string): Promise<IAccount> {
+      return new Promise((resolve) => resolve(null));
+    }
+  }
+  return new LoadAccountByEmailRepositoryStub();
+};
+
+interface ISutTypes {
+  sut: DbCreateAccount
+  hasherStub: IHasher
+  createAccountRepositoryStub: ICreateAccountRepository
+  loadAccountByEmailRepositoryStub: ILoadAccountByEmailRepository
+}
+
+const makeSut = (): ISutTypes => {
+  const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository();
+  const hasherStub = makeHasher();
+  const createAccountRepositoryStub = makeCreateAccountRepository();
+  const sut = new DbCreateAccount(
+    hasherStub,
+    createAccountRepositoryStub,
+    loadAccountByEmailRepositoryStub,
+  );
+  return {
+    sut,
+    hasherStub,
+    createAccountRepositoryStub,
+    loadAccountByEmailRepositoryStub,
+  };
+};
+
+describe('DbCreateAccount Usecase', () => {
   it('Should call Hasher with correct password', async () => {
+    const { sut, hasherStub } = makeSut();
     const hashSpy = jest.spyOn(hasherStub, 'hash');
     await sut.create(makeFakeAccountDTO());
     expect(hashSpy).toHaveBeenCalledWith('valid_password');
   });
 
   it('Should throw if Hasher throws', async () => {
+    const { sut, hasherStub } = makeSut();
     jest
       .spyOn(hasherStub, 'hash')
       .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())));
@@ -57,6 +90,7 @@ describe('DbCreateAccount Usecase', () => {
   });
 
   it('Should call CreateAccountRepository with correct values', async () => {
+    const { sut, createAccountRepositoryStub } = makeSut();
     const createSpy = jest.spyOn(createAccountRepositoryStub, 'create');
     await sut.create(makeFakeAccountDTO());
     expect(createSpy).toHaveBeenCalledWith({
@@ -67,6 +101,7 @@ describe('DbCreateAccount Usecase', () => {
   });
 
   it('Should throw if CreateAccountRepository throws', async () => {
+    const { sut, createAccountRepositoryStub } = makeSut();
     jest
       .spyOn(createAccountRepositoryStub, 'create')
       .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())));
@@ -75,12 +110,8 @@ describe('DbCreateAccount Usecase', () => {
   });
 
   it('Should return an account on success', async () => {
+    const { sut } = makeSut();
     const account = await sut.create(makeFakeAccountDTO());
-    expect(account).toEqual({
-      id: 'valid_id',
-      name: 'valid_name',
-      email: 'valid_email',
-      password: 'hashed_password',
-    });
+    expect(account).toEqual(makeFakeAccount());
   });
 });
