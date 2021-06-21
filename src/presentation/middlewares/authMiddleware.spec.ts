c560440469755ded01f4cceff3683f58b9980efc
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-21 19:34:36 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/presentation/middlewares/authMiddleware.spec.ts
+++ src/presentation/middlewares/authMiddleware.spec.ts
@@ -2,10 +2,43 @@ import { AccessDeniedError } from '../errors';
 import { forbidden } from '../helpers/http/http-helper';
 import { AuthMiddleware } from './AuthMiddleware';
 
+import { IAccount } from '../../domain/models/IAccount';
+import { ILoadAccountByToken } from '../../domain/usecases/ILoadAccountByToken';
+
+const makeFakeAccount = (): IAccount => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'hashed_password',
+});
+
 describe('Auth Middleware', () => {
   it('Should return 403 if no x-access-token exists in headers', async () => {
-    const sut = new AuthMiddleware();
+    class LoadAccountByTokenStub implements ILoadAccountByToken {
+      async load(accessToken: string, role?: string): Promise<IAccount> {
+        return new Promise((resolve) => resolve(makeFakeAccount()));
+      }
+    }
+    const loadAccountByTokenStub = new LoadAccountByTokenStub();
+    const sut = new AuthMiddleware(loadAccountByTokenStub);
     const httpResponse = await sut.handle({});
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
+  });
+
+  it('Should call LoadAccountByToken if correct accessToken', async () => {
+    class LoadAccountByTokenStub implements ILoadAccountByToken {
+      async load(accessToken: string, role?: string): Promise<IAccount> {
+        return new Promise((resolve) => resolve(makeFakeAccount()));
+      }
+    }
+    const loadAccountByTokenStub = new LoadAccountByTokenStub();
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load');
+    const sut = new AuthMiddleware(loadAccountByTokenStub);
+    await sut.handle({
+      headers: {
+        'x-access-token': 'any_token',
+      },
+    });
+    expect(loadSpy).toHaveBeenCalledWith('any_token');
   });
 });
