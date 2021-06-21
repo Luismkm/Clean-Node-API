65b88421ed07bbbd610a62137eb1478ec4805af8
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-21 14:12:31 -0300

    test: ensure SurveyMongoRepository create a new survey on success

---
+++ src/infra/database/mongodb/survey/SurveyMongoRepository.spec.ts
@@ -0,0 +1,40 @@
+import { Collection } from 'mongodb';
+import { MongoHelper } from '../helpers/mongoHelper';
+import { SurveyMongoRepository } from './SurveyMongoRepository';
+
+let sut: SurveyMongoRepository;
+let surveyCollection: Collection;
+
+describe('Survey Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    surveyCollection.deleteMany({});
+
+    sut = new SurveyMongoRepository();
+  });
+
+  it('Should create a survey on success', async () => {
+    await sut.create({
+      question: 'any_question',
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_answer',
+        },
+        {
+          answer: 'other_answer',
+        },
+      ],
+    });
+    const survey = await surveyCollection.findOne({ question: 'any_question' });
+    expect(survey).toBeTruthy();
+  });
+});
