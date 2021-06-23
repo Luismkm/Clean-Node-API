a0ebef9b16fa0b73ca1346c61428b1594a0bae99
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-23 13:53:39 -0300

    test: ensure JwtAdapter call verify with correct values

--- src/infra/criptography/jwtAdapter/jwtAdapter.spec.ts
+++ src/infra/criptography/jwtAdapter/jwtAdapter.spec.ts
@@ -6,6 +6,10 @@ jest.mock('jsonwebtoken', () => ({
   sign(): string {
     return 'any_token';
   },
+
+  verify(): string {
+    return 'any_value';
+  },
 }));
 
 const makeSut = (): JwtAdapter => new JwtAdapter('secret');
@@ -24,6 +28,15 @@ describe('JWT Adapter', () => {
       const signSpy = jest.spyOn(jwt, 'sign');
       sut.encrypt('any_id');
       expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
+    });
+  });
+
+  describe('verify()', () => {
+    it('Should call verify with correct values', () => {
+      const sut = makeSut();
+      const verifySpy = jest.spyOn(jwt, 'verify');
+      sut.decrypt('any_token');
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret');
     });
   });
 });
