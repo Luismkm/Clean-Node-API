cf4f52ee82bae744bc0dc32acded9760cec30a5a
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-24 17:14:49 -0300

    test: ensure SurveyMongoRepository load all surveys

--- src/infra/database/mongodb/survey/SurveyMongoRepository.spec.ts
+++ src/infra/database/mongodb/survey/SurveyMongoRepository.spec.ts
@@ -21,21 +21,54 @@ describe('Survey Mongo Repository', () => {
     sut = new SurveyMongoRepository();
   });
 
-  it('Should create a survey on success', async () => {
-    await sut.create({
-      question: 'any_question',
-      answers: [
-        {
-          image: 'any_image',
-          answer: 'any_answer',
-        },
-        {
-          answer: 'other_answer',
-        },
-      ],
-      date: new Date(),
+  describe('create()', () => {
+    it('Should create a survey on success', async () => {
+      await sut.create({
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image',
+            answer: 'any_answer',
+          },
+          {
+            answer: 'other_answer',
+          },
+        ],
+        date: new Date(),
+      });
+      const survey = await surveyCollection.findOne({ question: 'any_question' });
+      expect(survey).toBeTruthy();
     });
-    const survey = await surveyCollection.findOne({ question: 'any_question' });
-    expect(survey).toBeTruthy();
+  });
+
+  describe('loadAll()', () => {
+    it('Should load all surveys on success', async () => {
+      await surveyCollection.insertMany([
+        {
+          question: 'any_question',
+          answers: [
+            {
+              image: 'any_image',
+              answer: 'any_answer',
+            },
+          ],
+          date: new Date(),
+        },
+        {
+          question: 'other_question',
+          answers: [
+            {
+              image: 'other_image',
+              answer: 'other_answer',
+            },
+          ],
+          date: new Date(),
+        },
+      ]);
+      const surveys = await sut.loadAll();
+      expect(surveys.length).toBe(2);
+      expect(surveys[0].question).toBe('any_question');
+      expect(surveys[1].question).toBe('other_question');
+    });
   });
 });
