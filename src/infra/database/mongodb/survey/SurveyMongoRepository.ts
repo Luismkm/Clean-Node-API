08dc423c62378580cdd1c702d624aff05acefb68
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-24 17:14:30 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/database/mongodb/survey/SurveyMongoRepository.ts
+++ src/infra/database/mongodb/survey/SurveyMongoRepository.ts
@@ -2,10 +2,18 @@ import { MongoHelper } from '../helpers/mongoHelper';
 
 import { ICreateSurveyRepository } from '../../../../data/protocols/db/survey/ICreateSurveyRepository';
 import { ICreateSurveyDTO } from '../../../../domain/usecases/ICreateSurvey';
+import { ILoadSurveyRepository } from '../../../../data/protocols/db/survey/ILoadSurveysRepository';
+import { ISurvey } from '../../../../domain/models/ISurvey';
 
-export class SurveyMongoRepository implements ICreateSurveyRepository {
+export class SurveyMongoRepository implements ICreateSurveyRepository, ILoadSurveyRepository {
   async create(survey: ICreateSurveyDTO): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys');
     await surveyCollection.insertOne(survey);
+  }
+
+  async loadAll(): Promise<ISurvey[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys');
+    const surveys: ISurvey[] = await surveyCollection.find().toArray();
+    return surveys;
   }
 }
