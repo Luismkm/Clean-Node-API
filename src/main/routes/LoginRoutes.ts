68f84dd9cfe461e32010495b7b1aaec448d80041
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-24 08:48:17 -0300

    refactor: update paths

--- src/main/routes/LoginRoutes.ts
+++ src/main/routes/LoginRoutes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import { adaptRoute } from '../adapter/express/expressRouteAdapter';
 
-import { makeLoginController } from '../factories/controllers/login/loginControllerFactory';
-import { makeSignUpController } from '../factories/controllers/signup/signupControllerFactory';
+import { makeLoginController } from '../factories/controllers/login/login/loginControllerFactory';
+import { makeSignUpController } from '../factories/controllers/login/signup/signupControllerFactory';
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()));
