f8471b2c4434ee4ec497b5ad53f1451228ea3cf5
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-26 11:07:57 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult if its new

---
+++ src/infra/database/mongodb/surveyResult/surveyResultMongoRepository.ts
@@ -0,0 +1,24 @@
+import { MongoHelper } from '../helpers/mongoHelper';
+
+import { ISaveSurveyResultRepository } from '../../../../data/protocols/db/survey/ISaveSurveyResultRepository';
+import { ISurveyResult } from '../../../../domain/models/ISurveyResult';
+import { ISaveSurveyResultDTO } from '../../../../domain/usecases/ISaveSurveyResult';
+
+export class SurveyResultMongoRepository implements ISaveSurveyResultRepository {
+  async save(data: ISaveSurveyResultDTO): Promise<ISurveyResult> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults');
+    const res = await surveyResultCollection.findOneAndUpdate({
+      surveyId: data.surveyId,
+      accountId: data.accountId,
+    }, {
+      $set: {
+        answer: data.answer,
+        date: data.date,
+      },
+    }, {
+      upsert: true,
+      returnOriginal: false,
+    });
+    return res.value && MongoHelper.map(res.value);
+  }
+}
