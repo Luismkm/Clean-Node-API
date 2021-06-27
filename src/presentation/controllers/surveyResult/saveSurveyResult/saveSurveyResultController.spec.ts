c8bbfd07e486423d8b880f758e534a171ec460c8
Luismkm <luismarcelo13@yahoo.com.br>
2021-06-27 16:55:04 -0300

    test: ensure SaveSurveyResultController returns 200 on success

--- src/presentation/controllers/surveyResult/saveSurveyResult/saveSurveyResultController.spec.ts
+++ src/presentation/controllers/surveyResult/saveSurveyResult/saveSurveyResultController.spec.ts
@@ -1,7 +1,7 @@
 import MockDate from 'mockdate';
 
 import { InvalidParamError } from '../../../errors';
-import { forbidden, serverError } from '../../../helpers/http/http-helper';
+import { forbidden, serverError, success } from '../../../helpers/http/http-helper';
 import { SaveSurveyResultController } from './saveSurveyResultController';
 
 import {
@@ -133,5 +133,11 @@ describe('SaveSurveyResult Controller', () => {
       .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())));
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  it('Should return 200 if SaveSurveyResult throws', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(success(makeFakeSurveyResult()));
   });
 });
