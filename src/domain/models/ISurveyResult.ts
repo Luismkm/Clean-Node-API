48036268cc6d17c5b48beda7409c466c6ca655b9
Luismkm <luismarcelo13@yahoo.com.br>
2022-04-10 19:09:05 -0300

    refactor: update type

--- src/domain/models/ISurveyResult.ts
+++ src/domain/models/ISurveyResult.ts
@@ -1,7 +1,13 @@
+type SurveyResultAnswer = {
+  image?: string
+  answer: string
+  count: number
+  percent: number
+}
+
 export type ISurveyResult = {
-  id: string,
   surveyId: string,
-  accountId: string,
-  answer: string,
+  answers: SurveyResultAnswer[]
+  question: string,
   date: Date
 }
--- src/domain/usecases/surveyResult/ISaveSurveyResult.ts
+++ src/domain/usecases/surveyResult/ISaveSurveyResult.ts
@@ -1,6 +1,11 @@
 import { ISurveyResult } from '../../models/ISurveyResult';
 
-export type ISaveSurveyResultDTO = Omit<ISurveyResult, 'id'>
+export type ISaveSurveyResultDTO = {
+  surveyId: string,
+  accountId: string,
+  answer: string,
+  date: Date
+}
 
 export interface ISaveSurveyResult {
   save(data: ISaveSurveyResultDTO): Promise<ISurveyResult>
