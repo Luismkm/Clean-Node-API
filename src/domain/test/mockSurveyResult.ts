98ef7f305d0b507a4935c6f6ec2f00ce1ee75fac
Luismkm <luismarcelo13@yahoo.com.br>
2022-04-10 19:09:47 -0300

    refactor: update mockSurveyResult

--- src/domain/test/mockSurveyResult.ts
+++ src/domain/test/mockSurveyResult.ts
@@ -9,5 +9,17 @@ export const mockSaveSurveyResultDTO = (): ISaveSurveyResultDTO => ({
 });
 
 export const mockSurveyResult = (): ISurveyResult => ({
-  ...mockSaveSurveyResultDTO(), id: 'any_id',
+  surveyId: 'any_survey_id',
+  question: 'any_question',
+  answers: [{
+    answer: 'any_answer',
+    count: 1,
+    percent: 50,
+  }, {
+    answer: 'other_answer',
+    image: 'any_image',
+    count: 10,
+    percent: 80,
+  }],
+  date: new Date(),
 });
